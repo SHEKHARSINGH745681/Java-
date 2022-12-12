7e0bd347d1813c59a492f73c386edec5fdebdba8
SHEKHAR SINGH <107771203+SHEKHARSINGH745681@users.noreply.github.com>
2022-12-12 09:09:08 +0530

    Add files via upload

---
+++ swap_num.java
@@ -0,0 +1,16 @@
+package lec6;
+
+public class swap_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=7;
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);		
+
+	}
+
+}
